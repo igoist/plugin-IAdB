b97679184918e49d3158f200766acc21d78c8d57
igoist <493512132@qq.com>
2018-04-05 22:08:08 +0800

    *://lodash.com/*: hide #carbonads with !important

---
+++ src/lodash.js
@@ -0,0 +1,17 @@
+let body = document.body;
+
+let styleStr = `
+  #carbonads {
+    display: none !important;
+  }
+`;
+
+// 简单粗暴
+let trickStyle = document.createElement('style');
+trickStyle.id = 'sssss';
+trickStyle.type = 'text/css';
+trickStyle.innerHTML = styleStr;
+
+// No! it will be covered
+// body.insertBefore(trickStyle, body.children[0]);
+body.appendChild(trickStyle);
