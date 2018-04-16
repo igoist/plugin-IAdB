cd4cef3fed325cda079f56f61082f5819d49d1fa
igoist <493512132@qq.com>
2018-04-16 17:10:48 +0800

    *://*.baidu.com/*: 搜索结果页内容再移除、样式覆盖，百科页内容移除

--- src/baidu.js
+++ src/baidu.js
@@ -1,23 +1,74 @@
 // 闭包，后期再说
+const log = console.log.bind(this);
 
-// 搜索结果页右侧广告区, #content_right
+/*
+ * 搜索结果页
+ * 以 DOM 移除外加样式覆盖的方式
+ * #content_right 右侧广告区
+ * 搜索结果样式魔改
+ * #rs 底部相关搜索这个狗东西
+ */
 let contentRight = document.getElementById('content_right');
-
-// let kw = document.getElementById('kw');
-
-if (contentRight) {
-  contentRight.remove();
-}
+let rs = document.getElementById('rs');
+// let kw = document.getElementById('kw'); // 搜索输入框
 
 let styleStr = `
   #content_right {
     display: none;
   }
+
+  #container {
+    margin: 0 auto;
+    width: 1240px;
+  }
+
+  #content_left {
+    padding: 0 20px;
+    width: 100%;
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .c-container {
+    padding: 12px;
+    width: 588px;
+    min-height: 160px;
+    border: 1px solid transparent;
+    box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.16);
+    box-sizing: border-box;
+  }
+
+  #rs,
+  #rs_top_new,
+  .hit_top_new,
+  .leftBlock {
+    display: none!important;
+  }
+
+  h3.t a {
+    color: #149cec;
+  }
+
+  h3.t a em {
+    color: #ec414d;
+  }
 `;
+
+if (contentRight) {
+  contentRight.remove();
+  log('搜索结果页右侧广告区已移除');
+}
+
+if (rs) {
+  rs.remove();
+  log('底部相关搜索 #rs 已移除');
+}
 
 // 简单粗暴 -- 然后 Head 会刷新
 let trickStyle = document.createElement('style');
-trickStyle.id = 'sssss';
+trickStyle.id = 'igoist_iadb';
 trickStyle.type = 'text/css';
 trickStyle.innerHTML = styleStr;
 
@@ -51,3 +102,38 @@ body.insertBefore(trickStyle, body.children[0]);
 // kw.addEventListener('keydown', (e) => {
 //   document.getElementsByTagName('HEAD').item(0).appendChild(trickStyle);
 // });
+
+
+/*
+ * 百科页内容处理
+ * .side-content 右侧内容
+ * .topA 顶部广告
+ * .new-side-share 右侧分享按钮
+ * .after-content 底部猜你喜欢
+ */
+
+let sideContent = document.querySelector('.side-content');
+let topA = document.querySelector('.topA');
+let newSideShare = document.querySelector('.new-side-share');
+let afterContent = document.querySelector('.after-content');
+
+if (sideContent) {
+  sideContent.remove();
+  log('百科页右侧已移除');
+}
+
+
+if (topA) {
+  topA.remove();
+  log('百科页顶部广告 .topA 已移除');
+}
+
+if (newSideShare) {
+  newSideShare.remove();
+  log('百科页右侧分享 .new-side-share 已移除');
+}
+
+if (afterContent) {
+  afterContent.remove();
+  log('百科页底部猜你喜欢 .after-content 已移除');
+}
