0dc3edcc8d7c61271ad17de19d1a585eab6a425e
igoist <493512132@qq.com>
2018-05-14 18:37:23 +0800

    IAdB popup: UI update & control darkMode, color

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,53 +1,63 @@
-let changeColor = document.getElementById('changeColor');
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//       tabs[0].id,
+//       {
+//         code: `
+//           console.log(${JSON.stringify(chrome.tabs, null, 2)});
+//           console.log(${JSON.stringify(tabs, null, 2)});
+//           document.body.style.backgroundColor = '${color}';
+//           document.body.style.backgroundImage = 'none';
+//         `
+//       }
+//     );
+//   });
 
-let x = document.querySelectorAll('.item')[0];
-let x2 = document.querySelectorAll('.item')[1];
-let color;
+let fontColor;
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+let ifDarkMode;
+let ifFontColorSwitch;
+let lis = document.querySelectorAll('.nav-tags li');
 
-  color = data.color;
-  x.style.backgroundColor = data.color;
+chrome.storage.sync.get(['color', 'darkMode'], result => {
+  ifDarkMode = result.darkMode;
+  fontColor = result.color;
+  ifFontColorSwitch = fontColor === '#86c797' ? false : true;
+
+  console.log(lis);
+
+  if (ifDarkMode) {
+    lis[0].classList.add('active');
+  }
+
+  if (ifFontColorSwitch) {
+    lis[1].classList.add('active');
+  }
+
+  lis[0];
 });
 
-// setTimeout(() => {
-//   x.style.backgroundColor = '#333';
-// }, 2000);
 
+lis[0].addEventListener('click', e => {
+  e.preventDefault();
 
-x.addEventListener('click', function(e) {
-  x.style.backgroundColor = color;
-  console.log(chrome.tabs);
+  ifDarkMode = !ifDarkMode;
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {
-        code: `
-          console.log(${JSON.stringify(chrome.tabs, null, 2)});
-          console.log(${JSON.stringify(tabs, null, 2)});
-          document.body.style.backgroundColor = '${color}';
-          document.body.style.backgroundImage = 'none';
-        `
-      }
-    );
-  });
-});
-
-x2.addEventListener('click', e => {
   chrome.storage.sync.set({
-    color: '#86c797'
-  }, function() {
-    console.log("And the color is blue.");
-    // window.open('/pages/popup/index.html');
-    chrome.tabs.create({
-      url: '/pages/popup/index.html',
-      active: true
-    }, (tab) => {
-      console.log('new tab', tab);
-    });
+    darkMode: ifDarkMode
+  }, () => {
+    lis[0].classList.toggle('active');
   });
 });
 
+lis[1].addEventListener('click', e => {
+  e.preventDefault();
+
+  fontColor = !ifFontColorSwitch ? '#86c7c7' : '#86c797';
+
+  chrome.storage.sync.set({
+    color: fontColor
+  }, () => {
+    ifFontColorSwitch = !ifFontColorSwitch;
+    lis[1].classList.toggle('active');
+  });
+});
