a97a0a73d602af9c2171d650c1abfac037c39988
igoist <493512132@qq.com>
2019-03-22 18:14:48 +0800

    feature/key-menu: change the form of awaking keyMenu and prepare for the further cmds

--- src/oneForAll/index.js
+++ src/oneForAll/index.js
@@ -24,6 +24,7 @@ const IAdBState = {
    * backgroundColor
    * fontColor
    * cC 计数器
+   * keyArray 指令字符串
    * switchFlag 按键控制标记
    * darkMode 当前模式
    * ifBgImage 是否保留背景图
@@ -39,6 +40,7 @@ const IAdBState = {
   let ifReadCode;
 
   let cC = 0;
+  let keyArray = '';
   let idName = 'iadb_reset_site_style';
 
   const keyMenu = returnKeyMenu({});
@@ -57,6 +59,24 @@ const IAdBState = {
   // ];
 
   const KeyCodeArr = Object.keys(IAdBState);
+
+  const dispatchMenuTask = (keyArray) => {
+    switch (keyArray) {
+      case '000':
+        if (!keyMenu.state.doing) {
+          console.log('keyMenu state', keyMenu.state);
+          if (keyMenu.state.show) {
+            keyMenu.hide();
+          } else {
+            keyMenu.show();
+          }
+        }
+        return true;
+      default:
+        console.log('無駄ですよ');
+        return false;
+    }
+  };
 
   const returnStyleStr = (config) => {
     const { backgroundColor, fontColor, ifBgImage, ifNoImage, ifReadCode } = config;
@@ -118,7 +138,7 @@ const IAdBState = {
     // 以上为初始与执行 & 以下为持久存在事件
 
     let handleIClickEvent = e => {
-      console.log('e.key: ', e.key);
+      console.log('e.key: ', e.key, ' keyArray: ', keyArray);
       console.log('e.keyCode: ', e.keyCode);
       console.log('cC: ', cC, ' switchFlag:', switchFlag);
 
@@ -139,20 +159,6 @@ const IAdBState = {
         trickStyle.innerHTML = styleStr;
 
         if (result.programSwitch) {
-          if (e.altKey & !keyMenu.state.doing) {
-            // if (cC === 2 && !switchFlag) {
-            //   document.body.insertBefore(trickStyle, document.body.children[0]);
-            //   keyMenu.show();
-            //   console.log('Key menu!');
-            //   switchFlag = true;
-            // }
-            console.log('keyMenu state', keyMenu.state);
-            if (keyMenu.state.show) {
-              keyMenu.hide();
-            } else {
-              keyMenu.show();
-            }
-          }
           if (e.ctrlKey) {
             cC += 1;
 
@@ -160,8 +166,6 @@ const IAdBState = {
               document.body.insertBefore(trickStyle, document.body.children[0]);
               switchFlag = !switchFlag;
               cC = 0;
-
-              // Message.success('Switch Off');
             }
 
             if (cC === 2 && switchFlag) {
@@ -223,10 +227,21 @@ const IAdBState = {
             };
             xhr.send(formData);
             cC = 0;
+          } else if (document.activeElement.nodeName !== 'INPUT' && keyArray.length < 3 && (47 < e.keyCode && e.keyCode < 58)) {
+            // keyMenu 显示 && keyArray.length < 3 && 焦点非 input && key 0 ~ 9
+            keyArray += e.key;
+            if (keyArray.length === 3) {
+              // if order executed, then reset
+              if (dispatchMenuTask(keyArray)) {
+                keyArray = '';
+              } else {
+                keyArray = keyArray.slice(1, 3);
+              }
+            }
           } else {
+            keyArray = '';
             cC = 0;
           }
-          // console.log('cc: ', cC, darkMode, switchFlag);
         }
       });
     };
