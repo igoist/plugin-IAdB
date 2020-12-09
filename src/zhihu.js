293b14187e7eba00ffb69b679a810844acdd1bb8
igoist <493512132@qq.com>
2020-12-09 18:26:04 +0800

    *://www.zhihu.com/*: hide .QuestionHeader-main .QuestionHeader-title

--- src/zhihu.js
+++ src/zhihu.js
@@ -53,24 +53,42 @@ const mainF = function () {
   // }
 
   ////////////
+  let titleArr = ['.QuestionHeader-main .QuestionHeader-title'];
   let mainArr = ['.Topstory-mainColumn', '.SearchMain', '.Question-mainColumn', '.Profile-mainColumn', '.Collections-mainColumn'];
 
   let sideBarArr = ['.GlobalSideBar', '.SearchSideBar', '.Question-sideColumn', '.Profile-sideColumn'];
 
   const checkMS = () => {
     let i;
-    let x, y;
+    let w, x, y;
+
+    for (i = 0; i < titleArr.length; i++) {
+      let tmp = Q(titleArr[i]);
+
+      if (tmp) {
+        log.l({
+          title: 'checkMS',
+          text: `${titleArr[i]} exist`,
+        });
+        console.log(tmp);
+        w = {
+          c: titleArr[i],
+          d: tmp,
+        };
+      }
+    }
+
     for (i = 0; i < mainArr.length; i++) {
       let tmp = Q(mainArr[i]);
       if (tmp) {
         log.l({
           title: 'checkMS',
-          text: `${mainArr[i]} exist`
+          text: `${mainArr[i]} exist`,
         });
         console.log(tmp);
         x = {
           c: mainArr[i],
-          d: tmp
+          d: tmp,
         };
       }
     }
@@ -80,34 +98,38 @@ const mainF = function () {
       if (tmp) {
         log.l({
           title: 'checkMS',
-          text: `${sideBarArr[i]} exist`
+          text: `${sideBarArr[i]} exist`,
         });
         console.log(tmp);
         y = {
           c: sideBarArr[i],
-          d: tmp
+          d: tmp,
         };
       }
     }
 
-    return [x, y];
+    return [w, x, y];
   };
 
   let div = document.createElement('div');
   document.body.appendChild(div);
 
   const prefix = 'zh';
-  const [main, siderBar] = checkMS();
+  const [title, main, siderBar] = checkMS();
 
   const R = () => {
+    const [w, setW] = useState(title);
     const [x, setX] = useState(main);
     const [y, setY] = useState(siderBar);
-    const [a, setA] = useState(false);
-    const [b, setB] = useState(false);
+    const [fW, setFW] = useState(false);
+    const [fX, setFX] = useState(false);
+    const [fY, setFY] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     useEffect(() => {
-      hA();
-      hB();
+      // hA();
+      hFX();
+      hFY();
 
       let ViewAllBtns = QA('.Question-main .ViewAll');
       if (ViewAllBtns.length > 0) {
@@ -115,8 +137,8 @@ const mainF = function () {
           ViewAllBtns[i].addEventListener('click', () => {
             setTimeout(() => {
               const [main, siderBar] = checkMS();
-              setA(false);
-              setB(false);
+              setFX(false);
+              setFY(false);
               setX(main);
               setY(siderBar);
             }, 1000);
@@ -125,43 +147,70 @@ const mainF = function () {
       }
     }, []);
 
+    // useEffect(() => {
+    //   // console.log('w change: ', w.c, w && w.d.style.width, a);
+    //   hFW();
+    // }, [w]);
+
     useEffect(() => {
       // console.log('x change: ', x.c, x && x.d.style.width, a);
-      hA();
+      hFX();
     }, [x]);
 
     useEffect(() => {
       // console.log('y change: ', y.c, y && y.d.style.display, b);
-      hB();
+      hFY();
     }, [y]);
 
-    const hA = () => {
-      if (a) {
+    const hFW = () => {
+      if (fW) {
+        w.d.style.display = '';
+      } else {
+        w.d.style.display = 'none';
+      }
+      setFW(!fW);
+    };
+
+    const hFX = () => {
+      if (fX) {
         x.d.style.width = '';
       } else {
         x.d.style.width = '100%';
       }
-      setA(!a);
+      setFX(!fX);
     };
 
-    const hB = () => {
-      if (b) {
+    const hFY = () => {
+      if (fY) {
         y.d.style.display = '';
       } else {
         y.d.style.display = 'none';
       }
-      setB(!b);
+      setFY(!fY);
+    };
+
+    const handleToggle = () => {
+      setMenu(!menu);
     };
 
     return (
-      <div className='IAdB zhihu-handler'>
-        <div className={`IAdB ${prefix}-item ${a && 'active'}`} onClick={hA}>
-          <button class='IAdB xxx'>{x ? x.c : '...'}</button>
+      <>
+        <div className={`IAdB zhihu-handler ${menu && 'hidden'}`}>
+          <div className={`IAdB ${prefix}-item www ${fW && 'active'}`} onClick={hFW}>
+            <button className='IAdB'>{w ? w.d.className : '...'}</button>
+          </div>
+          <div className={`IAdB ${prefix}-item xxx ${fX && 'active'}`} onClick={hFX}>
+            <button className='IAdB'>{x ? x.c : '...'}</button>
+          </div>
+          <div className={`IAdB ${prefix}-item yyy ${fY && 'active'}`} onClick={hFY}>
+            <button className='IAdB'>{y ? y.c : '...'}</button>
+          </div>
         </div>
-        <div className={`IAdB ${prefix}-item ${b && 'active'}`} onClick={hB}>
-          <button class='IAdB yyy'>{y ? y.c : '...'}</button>
+        {/* <div className={`IAdB zhihu-handler-toggle ${!menu & 'hidden'}`}>Toggle</div> */}
+        <div className={`IAdB zhihu-handler-toggle`} onClick={handleToggle}>
+          Toggle
         </div>
-      </div>
+      </>
     );
   };
 
